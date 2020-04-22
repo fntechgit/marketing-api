636d7571c10eae8bf2793417ec1cb63088e4d889
smarcet <smarcet@gmail.com>
2020-04-22 12:17:10 -0300

    Fixed clone query
    Signed-off-by: smarcet <smarcet@gmail.com>

--- api/services/config_values_service.py
+++ api/services/config_values_service.py
@@ -5,7 +5,13 @@ class ConfigValuesService:
 
     def clone_from_to(self, from_show_id:int, to_show_id:int):
         with connection.cursor() as cursor:
-            res = cursor.execute("INSERT INTO config_values (created,modified,`key`,type,value,file,show_id) "
-                                 "SELECT now(),now(),`key`,type,value,file,%s FROM config_values AS CV where CV.show_id = %s ",
-                                 [to_show_id, from_show_id])
+            res = cursor.execute("""INSERT INTO config_values (created,modified,`key`,type,value,file,show_id) 
+                                 SELECT now(),now(),`key`,type,value,file,%s FROM config_values AS CV 
+                                 WHERE CV.show_id = %s AND
+                                 NOT EXISTAS (
+                                    SELECT 1 FROM config_values WHERE config_values.`key`= cv.`key` 
+                                    AND config_values.show_id = %s
+                                 ); 
+                                 """,
+                                 [to_show_id, from_show_id, to_show_id])
         return res
